e9c11d48ca1d7a90fe2b347138dc6bb159530699
yuanlianxi <675976614@qq.com>
2018-08-03 12:22:25 +0800

    3

---
+++ src/main/java/com/yuanlx/gittest1/Main.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C), 2015-2018, 比特易湃公司
+ * FileName: Main
+ * Author:   yuanlx
+ * Date:     2018/6/17 12:55
+ * Description: zhulei
+ */
+
+ package com.yuanlx.gittest1;
+
+/**
+ * zhulei 
+ *
+ * @author yuanlx
+ * @create 2018/6/17
+ */
+public class Main {
+    public static  void main ( String[] args )
+    {
+        System.out.println("hello world");
+    }
+}
---
+++ src/main/test/java/com/yuanlx/gittest1/MainTest.java
@@ -0,0 +1,12 @@
+package com.yuanlx.gittest1;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @org.junit.Test
+    public void main() {
+        System.out.println("hello test");
+        Main.main(null);
+    }
+}
